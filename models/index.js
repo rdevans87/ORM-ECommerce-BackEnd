af449592bf487b8d2b7d3be551cab78f19cc1eec
Ryan Evans <rdevans87@gmail.com>
2021-04-24 03:47:48 -0400

    still re-working sequelize assioctionss in index.js to sync with server.js

--- models/index.js
+++ models/index.js
@@ -4,17 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Product belongs to Category
+// Defines a Category association starting with a Product
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Category has many Products
+//Defines a Category having many Products to create multiple foreign keys in the 'Product' table
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Product is associated through more than one Tags (through ProductTag)
+ Product.belongsToMany(Tag, {
 
+ }),
 // Tags belongToMany Products (through ProductTag)
 
+//Package four models and export them as an object to import proper names
 module.exports = {
   Product,
   Category,
