e3ba3a24ac47e1e6f0e391a2f191e094c59e533c
Ryan Evans <rdevans87@gmail.com>
2021-04-24 08:57:12 -0400

    setting up fields an defines columns in product.js

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,9 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// define columns
 Category.init(
   {
-    // define columns
     id: {
     type: DataTypes.INTEGER,
     allowNull: false,
--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
+
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -7,10 +8,27 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// define columns
 Product.init(
   {
-    // define columns
-  },
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    }, 
+    product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+    price: {
+    type: DataTypes.DECIMAL,
+
+
+    }
+
+    
+    
   {
     sequelize,
     timestamps: false,
