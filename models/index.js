904ccb02d56e7bb63c71bb632120a1fe9674a65f
Ryan Evans <rdevans87@gmail.com>
2021-04-24 04:01:57 -0400

    updating foreign key with correct ids

--- models/index.js
+++ models/index.js
@@ -4,21 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Defines a Category association starting with a Product
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
-
 //Defines a Category having many Products to create multiple foreign keys in the 'Product' table
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Product is associated through more than one Tags (through ProductTag)
- Product.belongsToMany(Tag, {
+// Defines a Category association starting with a Product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
+// Product is associated with many Tags 
+ Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'category_id',
  }),
-// Tags belongToMany Products (through ProductTag)
+
+// Tags associated with many Products 
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'category_id',
+});
 
 //Package four models and export them as an object to import proper names
 module.exports = {
