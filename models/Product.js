9da6e6aa29a9593d0f6beaf329e1a27d1d4e3771
Ryan Evans <rdevans87@gmail.com>
2021-04-26 04:58:29 -0400

    cleaned up formatting in product.js

--- models/Product.js
+++ models/Product.js
@@ -1,63 +1,49 @@
 // import important parts of sequelize library
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 // define columns
 Product.init(
   {
     id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-    }, 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     product_name: {
-    type: DataTypes.STRING,
-    allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-    validate: {
-    isDecimal: true
-    }  
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 10,
-    Validate: {
-      isNumeric: true
-    }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        isNumeric: true
+      }
     },
     category_id: {
-    type: Datatypes.INTEGER,
-    reference: {
-      model: 'category',
-      key: 'id'
+      type: Datatypes.INTEGER,
+      reference: {
+        model: 'category',
+        key: 'id'
+      }
     }
-    }
-  }.
-    {
-
-
-    }
-
-    
-
-
-    }
-
-    }
-
-    
-    
+  },
   {
     sequelize,
     timestamps: false,
