de85118e6bb573afdc8ccaf0fbf0c7c3f9ad6d11
Ryan Evans <rdevans87@gmail.com>
2021-04-28 03:40:41 -0400

    error in terminal when running script, refactoring files from error

--- models/index.js
+++ models/index.js
@@ -4,26 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Defines a Category association starting with a Product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 //Defines a Category having many Products to create multiple foreign keys in the 'Product' table
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
-// Defines a Category association starting with a Product
-Product.belongsToOne(Category, {
-  foreignKey: 'category_id',
-});
 
 // Product is associated with many Tags 
  Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
+  through: { 
+    model: ProductTag,
+  }
  }),
 
 // Tags associated with many Products 
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+  }
+  
 });
 
 //Package four models and export them as an object to import proper names
